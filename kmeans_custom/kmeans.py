92b4895dbda67658fda62733a656f1e9f68bcd67
antonella-giovannetti <127109361+antonella-giovannetti@users.noreply.github.com>
2024-06-26 17:04:17 +0200

    add notebook

--- kmeans_custom/kmeans.py
+++ kmeans_custom/kmeans.py
@@ -1,74 +1,66 @@
 import numpy as np
-import seaborn as sns
-from sklearn.datasets import make_blobs
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-import random 
+from typing import List
 
-class KMeans:
-    def __init__(self, n_clusters, max_iter=300):
+class KMeansCustom:
+    def __init__(self, n_clusters: int, max_iteration: int = 300) -> None:
         self.n_clusters = n_clusters
-        self.max_iter = max_iter
+        self.max_iteration = max_iteration
+        self.centroids = np.array([])
 
-    def euclidean(self, point, data):
-        return np.sqrt(np.sum((point - data) ** 2, axis=1))
-    
-    def fit(self, X_train):
-        min_, max_ = np.min(X_train, axis=0), np.max(X_train, axis=0)
-        self.centroids = [random.uniform(min_, max_) for _ in range(self.n_clusters)]
+    def euclidean(self, point: np.ndarray, centroids: np.ndarray) -> np.ndarray:
+        """ 
+        Calculate the Euclidean distance between a point and all centroids.
+        
+        Input : 
+            point : np.ndarray : A single data point.
+            centroids : np.ndarray : All centroids.
+
+        Output :
+            np.ndarray : Euclidean distance between a point and all centroids.
+        """
+        return np.sqrt(np.sum((point - centroids) ** 2, axis=1))
+
+    def fit(self, data: np.ndarray) -> None:
+        """
+        Fit the KMeansCustom model to the data.
+
+        Input : 
+            data : np.ndarray : Data to fit the KMeansCustom.
+
+        Output :
+            None
+        """
+        min_, max_ = np.min(data, axis=0), np.max(data, axis=0)
+        self.centroids = np.array([np.random.uniform(min_, max_) for _ in range(self.n_clusters)])
         iteration = 0
-        prev_centroids = None
-        while np.not_equal(self.centroids, prev_centroids).any and iteration < self.max_iter:
-            sorted_points = [[] for _ in range(self.n_clusters)]
-            for x in X_train:
-                dists = self.euclidean(x, self.centroids)
-                centroid_idx = np.argmin(dists)
-                sorted_points[centroid_idx].append(x)
-            prev_centroids = self.centroids
-            self.centroids = [np.mean(points, axis=0) for points in sorted_points]
+        previous_centroids = None 
+        while np.not_equal(self.centroids, previous_centroids).any() and iteration < self.max_iteration:
+            values_per_nearest_centroid = [[] for _ in range(self.n_clusters)] 
+            for datum in data:
+                distances = self.euclidean(datum, self.centroids)
+                nearest_centroid_index = np.argmin(distances)
+                values_per_nearest_centroid[nearest_centroid_index].append(datum)
+            previous_centroids = self.centroids.copy()
+            self.centroids = np.array([np.mean(values, axis=0) for values in values_per_nearest_centroid])
             for i, centroid in enumerate(self.centroids):
                 if np.isnan(centroid).any():
-                    self.centroids[i] = prev_centroids[i]
+                    self.centroids[i] = previous_centroids[i]
             iteration += 1
 
-    def evaluate(self, X):
-        centroids = []
-        centroid_idxs = []
-        for x in X:
-            dists = self.euclidean(x, self.centroids)
-            centroid_idx = np.argmin(dists)
-            centroids.append(self.centroids[centroid_idx])
-            centroid_idxs.append(centroid_idx)
-        return centroids, centroid_idx
+    def predict(self, data: np.ndarray) -> List[int]:
+        """
+        Predict the cluster index for each data point.
 
+        Input : 
+            data : np.ndarray : Data to predict the cluster index.
 
-centers = 5
-X_train, true_labels = make_blobs(n_samples=100, centers=centers, random_state=42)
-X_train = StandardScaler().fit_transform(X_train)
-print(X_train)
-sns.scatterplot(x=[X[0] for X in X_train],
-                y=[X[1] for X in X_train],
-                hue=true_labels,
-                palette="deep",
-                legend=None
-                )
-plt.xlabel("x")
-plt.ylabel("y")
-# plt.show()
-
-kmeans = KMeans(n_clusters=centers)
-kmeans.fit(X_train)
-class_centers, classification = kmeans.evaluate(X_train)
-sns.scatterplot(x=[X[0] for X in X_train],
-                y=[X[1] for X in X_train],
-                hue=true_labels,
-                style=classification,
-                palette="deep",
-                legend=None
-                )
-plt.plot([x for x, _ in kmeans.centroids],
-        [y for _, y in kmeans.centroids],
-        '+',
-        markersize=10,
-        )
-plt.show()
+        Output :
+            List[int] : Cluster index for each data point.
+        """
+        centroids_index = [] 
+        for datum in data:
+            distances = self.euclidean(datum, self.centroids)
+            centroid_index = np.argmin(distances)
+            centroids_index.append(centroid_index)
+        return centroids_index
+    
--- kmeans_custom/test.py
+++
@@ -1,5 +0,0 @@
-from sklearn import datasets
-import matplotlib.pyplot as plt
-
-iris = datasets.load_iris()
-print(iris.data)
